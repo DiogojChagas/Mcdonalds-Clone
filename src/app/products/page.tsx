df1a678cd55a59a3369b3b1b7e842053b9d0b535
unknown <diogolapapa@gmail.com>
2026-02-28 16:52:13 -0300

    fed: add shadcn initial components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductsPage = () => {
+  return (
+    <div className="p-5">
+      <h1 className="text-red-500">Products Page</h1>
+      <Button>Login</Button>
+    </div>
+  );
+};
+
+export default ProductsPage;
